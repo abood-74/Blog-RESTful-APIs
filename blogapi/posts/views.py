de517f3adc79901eff24875866268ddd645adc41
abood-74 <abdelrahmanashour45@gmail.com>
2023-04-27 07:48:45 +0200

    adding token authntication

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,14 +1,13 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import permissions
+from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
