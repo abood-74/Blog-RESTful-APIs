27b0ed6e80415a300f842bdf9c90525054d04f0b
abood-74 <abdelrahmanashour45@gmail.com>
2023-04-29 08:05:56 +0300

    adding view sets and routers

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,13 +1,15 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets # new
 from .models import Post
-from .serializers import PostSerializer
 from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet): # new
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet): # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
